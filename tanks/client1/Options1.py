5f65324f77bea87db1093a8e3b6c01a4f8738d3e
Ruslan <RuslanFedorov303@gmail.com>
2026-07-06 15:29:01 +0300

    Add files via upload

---
+++ tanks/client1/Options1.py
@@ -0,0 +1,136 @@
+import pygame
+from socket import *
+import json
+
+
+HOST = 'localhost'
+IP = 8081
+
+
+screen_size = {'width': 1300, 'height': 1000}
+
+camera_offset = {'x_offset': 0, 'y_offset': 0}
+camera_scale = 2000
+
+# screen = pygame.Surface((3000, 3000))
+screen = pygame.display.set_mode((screen_size['width'], screen_size['height'])) # Создаем екран и время
+clock = pygame.time.Clock()
+
+
+body_image = pygame.transform.scale(
+        pygame.image.load("Image/Tank_body.png").convert_alpha(),
+        (600, 300))
+
+turret_image = pygame.transform.scale(
+        pygame.image.load("Image/Tank_turret.png").convert_alpha(),
+        (600, 300))
+
+gun_image = pygame.transform.scale(
+        pygame.image.load("Image/Tank_gun.png").convert_alpha(),
+        (600, 300))
+
+projectile_image = pygame.transform.scale(
+        pygame.image.load("Image/Projectile.png").convert_alpha(),
+        (600, 300))
+
+bush_image = pygame.transform.scale(
+        pygame.image.load("Image/Bush.png").convert_alpha(),
+        (600, 300))
+
+
+# fon1_image = pygame.transform.scale(
+#         pygame.image.load("Image/fon_1.png").convert_alpha(),
+#         (100, 100))
+#
+# fon2_image = pygame.transform.scale(
+#         pygame.image.load("Image/fon_2.png").convert_alpha(),
+#         (100, 100))
+#
+# fon3_image = pygame.transform.scale(
+#         pygame.image.load("Image/fon_3.png").convert_alpha(),
+#         (100, 100))
+
+
+fon1_image = pygame.image.load("Image/fon_1.png").convert_alpha()
+
+fon2_image = pygame.image.load("Image/fon_2.png").convert_alpha()
+
+fon3_image = pygame.image.load("Image/fon_3.png").convert_alpha()
+
+fons = {'fon1': fon1_image, 'fon2': fon2_image, 'fon3': fon3_image}
+
+
+buch1_image = pygame.image.load("Image/Bush.png").convert_alpha()
+
+decorations = {'bush1': buch1_image}
+
+
+
+def drawTanks(data_players, camera_offset, my_id):
+    for data_player in data_players:
+        if data_player['tank_id'] == my_id:
+            x_coord = screen_size['width'] / 2
+            y_coord = screen_size['height'] / 2
+
+        else:
+            x_coord = screen_size['width'] / 2 + (data_player['x_coord'] - camera_offset['x_offset'])
+            y_coord = screen_size['height'] / 2 + (data_player['y_coord'] - camera_offset['y_offset'])
+
+
+        rotated_body = pygame.transform.rotate(body_image, data_player['body_rotate'])
+        body_rect = rotated_body.get_rect(center=(x_coord, y_coord))
+        screen.blit(rotated_body, body_rect)
+
+
+
+    for data_player in data_players:
+        if data_player['tank_id'] == my_id:
+            x_coord = screen_size['width'] / 2
+            y_coord = screen_size['height'] / 2
+
+        else:
+            x_coord = screen_size['width'] / 2 + (data_player['x_coord'] - camera_offset['x_offset'])
+            y_coord = screen_size['height'] / 2 + (data_player['y_coord'] - camera_offset['y_offset'])
+
+
+        rotated_gun = pygame.transform.rotate(gun_image, data_player['turret_rotate'])
+        gun_rect = rotated_gun.get_rect(center=(x_coord, y_coord))
+        screen.blit(rotated_gun, gun_rect)
+
+        rotated_turret = pygame.transform.rotate(turret_image, data_player['turret_rotate'])
+        turret_rect = rotated_turret.get_rect(center=(x_coord, y_coord))
+        screen.blit(rotated_turret, turret_rect)
+
+
+
+
+def drawProjectiles(data_projectiles, camera_offset):
+    for data_projectile in data_projectiles:
+        rotated_projectile = pygame.transform.rotate(projectile_image, data_projectile['rotate'])
+        rect_projectile = rotated_projectile.get_rect(center=(screen_size['width'] / 2 + (data_projectile['x_coord'] - camera_offset['x_offset']),
+                                                              screen_size['height'] / 2 + (data_projectile['y_coord'] - camera_offset['y_offset'])))
+        screen.blit(rotated_projectile, rect_projectile)
+
+
+
+def drawFons(fon_images, camera_offset):
+    for current_fon in fon_images:
+        scaled_fon = pygame.transform.scale(fons[current_fon['image']], (current_fon['width'], current_fon['height']))
+        rotated_fon = pygame.transform.rotate(scaled_fon, current_fon['rotate'])
+
+        rect_fon = rotated_fon.get_rect(
+            center=(screen_size['width'] / 2 + (current_fon['x_coord'] - camera_offset['x_offset']),
+                    screen_size['height'] / 2 + (current_fon['y_coord'] - camera_offset['y_offset'])))
+        screen.blit(rotated_fon, rect_fon)
+
+
+
+def drawDecorations(dicorations_list, camera_offset):
+    for current_decorate in dicorations_list:
+        scaled_decorate = pygame.transform.scale(decorations[current_decorate['image']], (current_decorate['width'], current_decorate['height']))
+        rotated_decorate = pygame.transform.rotate(scaled_decorate, current_decorate['rotate'])
+
+        rect_decorate = rotated_decorate.get_rect(
+            center=(screen_size['width'] / 2 + (current_decorate['x_coord'] - camera_offset['x_offset']),
+                    screen_size['height'] / 2 + (current_decorate['y_coord'] - camera_offset['y_offset'])))
+        screen.blit(rotated_decorate, rect_decorate)
---
+++ tanks/client1/User_tanks1.py
@@ -0,0 +1,100 @@
+import pygame
+from socket import *
+import json
+from Options1 import drawTanks, drawProjectiles, drawFons, drawDecorations, screen, screen_size
+
+
+
+HOST = 'localhost'
+IP = 8081
+
+
+socket_player = socket(AF_INET, SOCK_STREAM)
+socket_player.connect((HOST, IP))
+
+
+# my_id = socket_player.recv(2048).decode().strip()
+#
+# my_id = json.loads(my_id)
+#
+# my_id = my_id['id']
+
+my_id = json.loads(socket_player.recv(2048).decode().strip())['id']
+
+
+
+
+
+while True:
+    events = []
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                events.append('MOUSELEFTKEY')
+
+            elif event.button == 4:
+                camera_scale += 5
+
+            elif event.button == 5:
+                camera_scale -= 5
+
+
+    screen.fill((255, 255, 255))
+
+
+    try:
+        keys = pygame.key.get_pressed()
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+
+        if keys[pygame.K_w]: events.append('K_w')
+        if keys[pygame.K_s]: events.append('K_s')
+        if keys[pygame.K_a]: events.append('K_a')
+        if keys[pygame.K_d]: events.append('K_d')
+
+        if keys[pygame.K_LEFT]: events.append('K_LEFT')
+        if keys[pygame.K_RIGHT]: events.append('K_RIGHT')
+
+        if keys[pygame.K_UP]: events.append('K_UP')
+
+        socket_player.send(
+            json.dumps(
+                {'keys': events,
+                 'mouse_x': mouse_x - screen_size['width'] // 2,
+                 'mouse_y': mouse_y - screen_size['height'] // 2}
+            ).encode()
+        )
+
+        information_received = socket_player.recv(2048).decode().strip()
+
+        information_received = json.loads(information_received)
+
+
+        for data_player in information_received['tanks']:
+            if data_player['tank_id'] == my_id:
+                camera_offset = {'x_offset': data_player['x_coord'], 'y_offset': data_player['y_coord']}
+                break
+
+
+        drawFons(information_received['map']['fon_images'], camera_offset)
+        drawProjectiles(information_received['projectiles'], camera_offset)
+        drawTanks(information_received['tanks'], camera_offset, my_id)
+        drawDecorations(information_received['map']['decorations_list'], camera_offset)
+
+        # scaled_screen = pygame.transform.scale(screen, (camera_scale, camera_scale))
+        # true_screen.blit(scaled_screen, (screen_size['width'] / 2, screen_size['height'] / 2))
+        # true_screen.blit(scaled_screen, (screen_size['width'] / 2 + camera_offset['x_offset'],
+        #                                  screen_size['height'] / 2 + camera_offset['y_offset']))
+
+    except timeout:
+        continue
+
+
+    except Exception as ex:
+        print(ex)
+
+
+
+    pygame.display.flip()
